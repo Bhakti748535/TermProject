91f95dca96cd6197be4e98ce71a18b3b601fcdb9
Bhakti Bhakti <748535@student.tor.loc>
2019-08-07 17:02:38 -0400

    4th

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ const site = http.createServer(function(req, res) {
 site.listen(3000);*/
 
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/', function(req, res) {
     res.send('<h1>hello guys</h1>')
 })
